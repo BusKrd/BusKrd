27ea0ca45f4997be33365cb230b58c17e3590444
projectsCS <125819016+TheF4ntasticFour@users.noreply.github.com>
2025-01-22 07:43:30 +0300

    .

--- buskrd/lib/navigators/bottomNavigationBar.dart
+++ buskrd/lib/navigators/bottomNavigationBar.dart
@@ -14,29 +14,30 @@ class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed, 
       currentIndex: widget.selectedIndex,
       onTap: widget.onItemTapped,
-      selectedItemColor: Colors.blue, // Color for selected item
-      unselectedItemColor: Colors.grey,
+      selectedItemColor: const Color.fromARGB(255, 255, 0, 0), // Color for selected item
+      unselectedItemColor: const Color.fromARGB(255, 0, 0, 0),
       items: const [
         BottomNavigationBarItem(
-          icon: Icon(Icons.home_filled, color: Color.fromARGB(255, 0, 0, 0)), // Custom Home Icon
+          icon: Icon(Icons.home_filled, color: Color.fromARGB(255, 156, 39, 176),), // Custom Home Icon
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.route, color: Color.fromARGB(255, 0, 0, 0)), // Custom Route Icon
+          icon: Icon(Icons.route, color: Color.fromARGB(255, 156, 39, 176),), // Custom Route Icon
           label: 'Route',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.book, color: Color.fromARGB(255, 0, 0, 0)), // Custom Reservation Icon
+          icon: Icon(Icons.book, color: Color.fromARGB(255, 156, 39, 176),), // Custom Reservation Icon
           label: 'Reservation',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.notifications_active, color: Color.fromARGB(255, 0, 0, 0)), // Custom Notification Icon
+          icon: Icon(Icons.notifications_active, color: Color.fromARGB(255, 156, 39, 176),), // Custom Notification Icon
           label: 'Notifications',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person, color: Color.fromARGB(255, 0, 0, 0)), // Custom Profile Icon
+          icon: Icon(Icons.person, color: Color.fromARGB(255, 156, 39, 176),), // Custom Profile Icon
           label: 'Profile',
         ),
       ],
